d3f50687729ac9641bd06ca156ac9bf5fe7bebbb
Omar Calderon <stivenomar7@gmail.com>
2020-01-28 09:31:55 -0500

    Corregido obtencion de datos

--- js/app.js
+++ js/app.js
@@ -3,25 +3,40 @@ let anioMaximo;
 let anioMinimo;
 let rangoAnioAutos;
 let optionSelected;
+let buttonSelected;
+let tipoSeguro;
+let formularioEnvio;
+
 anioMaximo = new Date().getFullYear(); //anioActual
 anioMinimo = anioMaximo - 20;
 
+formularioEnvio = document.getElementById('formulario');
 rangoAnioAutos = document.getElementById('rangoAnio');
 optionSelected = document.getElementById('select');
+tipoSeguro = document.getElementById('tipoSeguro');
 
-console.log(optionSelected);
+formularioEnvio.addEventListener('submit',enviarDatos);
 
-window.addEventListener('DOMContentLoaded',cargarValores);
-rangoAnioAutos.addEventListener('click',verInfo);
 
 function cargarValores(event){
     rangoAnioAutos.max = anioMaximo;
     rangoAnioAutos.min = anioMinimo;
 }
 
-function verInfo(event){
-    console.log(event.target.value);
-    console.log(optionSelected.getAttribute('seleccion'));
+function enviarDatos(event){
+    event.preventDefault();
+    let marcaAuto = optionSelected.getAttribute('seleccion');
+    let anioAuto = rangoAnioAutos.value;
+    let seguroAuto = tipoSeguro.querySelector('input[name="group1"]:checked').value;
+
 }
 
+//clases
 
+class Auto {
+    constructor(marca,anio,tipoSeguro){
+        this.marca = marca;
+        this.anio = anio;
+        this.tipoSeguro = tipoSeguro;
+    }
+}
