444c17fffa0a8a53537de884c539a680ce7733fd
Omar Calderon <stivenomar7@gmail.com>
2020-01-28 22:01:40 -0500

    Creado el objeto y su metodo para calcular la cotizacion

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ rangoAnioAutos = document.getElementById('rangoAnio');
 optionSelected = document.getElementById('select');
 tipoSeguro = document.getElementById('tipoSeguro');
 
+
 formularioEnvio.addEventListener('submit',enviarDatos);
 
 
@@ -28,10 +29,27 @@ function enviarDatos(event){
     let marcaAuto = optionSelected.getAttribute('seleccion');
     let anioAuto = rangoAnioAutos.value;
     let seguroAuto = tipoSeguro.querySelector('input[name="group1"]:checked').value;
+    const nuevatarjeta = new tarjetaCotizacion();
+    if(marcaAuto == 0){
+        nuevatarjeta.mostrarError('Debes seleccionar una marca de auto');
+    }else {
+        const nuevoAuto = new Auto(marcaAuto,anioAuto,seguroAuto);
+        console.log(nuevoAuto.cotizarSeguro());
+    }
 
 }
 
 //clases
+class tarjetaCotizacion {
+
+    mostrarResultados(){
+
+    }
+
+    mostrarError(mensaje){
+        alert(mensaje)
+    }
+}
 
 class Auto {
     constructor(marca,anio,tipoSeguro){
@@ -39,4 +57,47 @@ class Auto {
         this.anio = anio;
         this.tipoSeguro = tipoSeguro;
     }
+
+    cotizarSeguro(){
+        /*
+        1 = americano 1.15
+        2 = asiatico  1.05
+        3 = europeo 1.35
+        */
+        let valorPorMarca;
+        let base;
+        let diferenciaDeAnios;
+        let valorTotal;
+
+        base = 2000;
+
+        switch (this.marca) {
+            case '1':
+                valorPorMarca = base * 1.15
+                break;
+            case '2':
+                valorPorMarca = base * 1.05
+                break;
+            case '3':
+                valorPorMarca = base * 1.35
+                break;
+        }
+
+        //obtener la diferencia entre el anio actual y el anio del auto
+
+        diferenciaDeAnios = new Date().getFullYear() - this.anio;
+
+        //por cada anio se reduce 3% al valor por marca
+        valorTotal = valorPorMarca -  (valorPorMarca * (diferenciaDeAnios * 3))/100;
+
+        if(this.tipoSeguro === 'Basico'){
+            valorTotal = valorTotal + (valorTotal * 0.30);
+        }else {
+            valorTotal = valorTotal + (valorTotal * 0.50);
+        }
+        
+        console.log(valorTotal);
+
+    }
+
 }
